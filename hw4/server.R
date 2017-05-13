811455fc9f5ac89c630269007711c005ceba99db
sflll <104071001@nccu.edu.tw>
2017-05-13 16:53:03 +0800

    add plot

--- hw4/server.R
+++ hw4/server.R
@@ -10,10 +10,53 @@ function(input, output) {
     read.csv(file.path(getwd(),'methods',infile), header = T, sep = ',')
   })
 
-  output$plot <- renderPlot({
-      
+  output$display_plot <- renderPlot({
+    target <- input$select_target
+    files <- input$select_plot
+    if(is.null(files)) {
+      return()
+    }
     
-     
+    sens <- c()
+    spes <- c()
+    
+    for(file in files)
+    {
+      tp = 0
+      tn = 0
+      fp = 0
+      fn = 0
+      name<-gsub(".csv", "", basename(file))
+      fname <- c(fname, name)
+      d<-read.table(file.path(getwd(),'methods',file), header=T,sep=",")
+      for(i in 1:dim(d)[1])
+      {
+        if(target == d[i,2]){
+          if(d[i,2] == d[i,3]){
+            tp <- tp+1
+          }else{
+            fp <- fp+1
+          }
+        }else{
+          if(d[i,2] == d[i,3]){
+            tn <- tn+1
+          }else{
+            fn <- fn+1
+          }
+        }
+        i <- i+1
+      }
+      sensitivity = round(tp/(tp+fn), 2)
+      specificity = round(tn/(tn+fp), 2)
+      sens <- c(sens, sensitivity)
+      spes <- c(spes, specificity)
+    }
+    specificity <- spes
+    sensitivity <- sens
+    data <- data.frame(specificity, sensitivity)
+    # show(data)
+    qplot(specificity, sensitivity, data = data)
+    
   })
     
   output$display_text <- renderText({
@@ -24,9 +67,3 @@ function(input, output) {
   })
 
 }
-
-# getSensitivity <- function()
-# {
-#   
-#   
-# }
--- hw4/ui.R
+++ hw4/ui.R
@@ -9,20 +9,26 @@ fluidPage(
   # Application title
   titlePanel("DataScience HW4!"),
   
-  # Sidebar with methods of options
+  # Sidebar with options
   sidebarLayout(
     sidebarPanel(
-      radioButtons('select_meths', 'Select methods to display', 
+      radioButtons('select_target', 'Select target', 
+                   choices = c('Male'='male', 'Female'='female')),
+      checkboxGroupInput('select_plot', 'Select methods to plot', 
+                         choices = c("Method 1" = "method1.csv", "Method 2" = "method2.csv", 
+                                        "Method 3" = "method3.csv", "Method 4" = "method4.csv", 
+                                        "Method 5" = "method5.csv", "Method 6" = "method6.csv", 
+                                        "Method 7" = "method7.csv", "Method 8" = "method8.csv", 
+                                        "Method 9" = "method9.csv", "Method 10" = "method10.csv")),
+      radioButtons('select_meths', 'Select methods to display content', 
                          choices = meths)
-      # chekcboxGroupInput('select_target')
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      # plotOutput("distPlot"),
+      plotOutput("display_plot"),
       textOutput("display_text"),
       tableOutput("method_content")
-      
       
     )
   )
